ffcc4d68f08761b51cff2daf85d95119d1acfdf1
quentin chabot <quentin.chabot@outlook.fr>
2019-12-04 09:07:01 +0100

    first commit

---
+++ src/Controller/MainController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Produit;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/main", name="main")
+     */
+    public function index()
+    {
+    /*         $toto = 'Ma variable toto';
+        return $this->render('main/index.html.twig', [
+            'toto' => $toto,
+        ]); */
+    }
+
+    /**
+     * @Route("/test", name="test")
+     */
+    public function test(): Response
+    {
+        $produit = new Produit();
+        $produit->setlibelle('PC');
+        $produit->setprix(1999);
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($produit);
+        $entityManager->flush();
+        return new Response('Saved new product with id '.$produit->getId());
+    }
+}
